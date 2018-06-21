29c0a70d8e2b7115e41e69d35c0916c7f1c01285
milos2806 <milosvasiljevic28@gmail.com>
2018-06-22 00:43:20 +0200

    added testCreateNewCategory and testEditCategory

--- test/category/TestCategories.java
+++ test/category/TestCategories.java
@@ -1,6 +1,10 @@
 package category;
 
 import framework.Helper;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
 import org.junit.Assert;
 import org.junit.After;
 import org.junit.AfterClass;
@@ -18,25 +22,19 @@ public class TestCategories {
 
     public static WebDriver driver;
     public static WebDriverWait wait;
+    public static DateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
 
     @BeforeClass
     public static void setUpClass() {
+
+//        dateFormat = new SimpleDateFormat("HH:mm:ss") ;
+        System.out.println("@BeforeClass: " + dateFormat.format(new Date()));
 
         driver = new ChromeDriver();
         wait = new WebDriverWait(driver, 10);
 
         driver.manage().window().maximize();
         driver.get("http://bvtest.school.cubes.rs/login");
-    }
-
-    @AfterClass
-    public static void tearDownClass() throws InterruptedException {
-//        Thread.sleep(3000);
-//        driver.quit();
-    }
-
-    @Before
-    public void setUp() {
 
         WebElement emailField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("email")));
         emailField.sendKeys("qa@cubes.rs");
@@ -48,6 +46,19 @@ public class TestCategories {
         loginButton.click();
 
         System.out.println("Page title is: " + driver.getTitle());
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws InterruptedException {
+        Thread.sleep(3000);
+        System.out.println("@AfterClass: " + dateFormat.format(new Date()));
+
+        driver.quit();
+    }
+
+    @Before
+    public void setUp() {
+        System.out.println("@Before: " + dateFormat.format(new Date()));
 
         WebElement categories = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id='app-navbar-collapse']/ul[1]/li[3]/a")));
         categories.click();
@@ -55,17 +66,56 @@ public class TestCategories {
     }
 
     @After
-    public void tearDown() {
+    public void tearDown() throws InterruptedException {
+        Thread.sleep(1000);
+        System.out.println("@After: " + dateFormat.format(new Date()));
+
     }
 
     @Test
     public void testCreateNewCategory() {
-        
-        for (int i = 0; i < 5; i++) {
+
+        for (int i = 0; i < 3; i++) {
             WebElement addCategoryButton = driver.findElement(By.className("pull-right"));
-        addCategoryButton.click();
+            addCategoryButton.click();
+
+            WebElement titleField = driver.findElement(By.id("title"));
+            titleField.sendKeys(Helper.getRandomText());
+
+            WebElement saveButton = driver.findElement(By.id("save-category-button"));
+            saveButton.click();
+
+            String expectedUrl = "http://bvtest.school.cubes.rs/admin/categories";
+            String actualUrl = driver.getCurrentUrl();
+
+            Assert.assertEquals("Url se ne poklapa", expectedUrl, actualUrl);
+
+            String expectedTitle = "Brze vesti admin  | Categories ".replaceAll("\\s+", " ").trim();
+//        System.out.println("expected title: '" + expectedUrl + "'");
+            String actualTitle = driver.getTitle();
+//        System.out.println("actual title: '" + expectedTitle + "'");
+
+            Assert.assertEquals("Title doesn't match", expectedTitle, actualTitle);
+        }
+
+    }
+
+    @Test
+    public void testEditCategory() {
+//            WebElement table = driver.findElement(By.id("categoriesTable"));
+        WebElement tBody = wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("ui-sortable")));
+        List<WebElement> rows = tBody.findElements(By.tagName("tr"));
+
+        System.out.println("number of rows: " + rows.size());
+
+        WebElement lastRow = rows.get(rows.size() - 4);
+
+        WebElement editButton = lastRow.findElement(By.className("btn-default"));
+        editButton.click();
 
         WebElement titleField = driver.findElement(By.id("title"));
+        // titleField.clear(); ---> sluzi da ocisti title polje da bi mogli da unesemo novu vrednost
+        titleField.clear();
         titleField.sendKeys(Helper.getRandomText());
 
         WebElement saveButton = driver.findElement(By.id("save-category-button"));
@@ -73,7 +123,7 @@ public class TestCategories {
 
         String expectedUrl = "http://bvtest.school.cubes.rs/admin/categories";
         String actualUrl = driver.getCurrentUrl();
-        
+
         Assert.assertEquals("Url se ne poklapa", expectedUrl, actualUrl);
 
         String expectedTitle = "Brze vesti admin  | Categories ".replaceAll("\\s+", " ").trim();
@@ -82,8 +132,10 @@ public class TestCategories {
 //        System.out.println("actual title: '" + expectedTitle + "'");
 
         Assert.assertEquals("Title doesn't match", expectedTitle, actualTitle);
-        }
-
-        
     }
+
+    @Test
+        public void testDeletCategory() {
+            
+        }
 }
